d354581121daaceab06a753037d1aa9953b9bf18
ApenasUmSonhador <123214565+ApenasUmSonhador@users.noreply.github.com>
2023-03-22 15:47:00 -0300

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -3,7 +3,7 @@ numero_2=0
 operador=''
 while True:
     numero_1= input("Digite um numero: ")
-    operador= input("Digite um operador: ")
+    operador= input("Digite um operador (entre: +, -,*,/, ^): ")
     numero_2= input("Digite outro numero: ")
     while not numero_1.isnumeric() or not numero_2.isnumeric():
         print("Tente novamente, os numeros foram considerados invalidos.")
@@ -12,20 +12,22 @@ while True:
         numero_2= input("Digite outro numero: ")
     num_1=float(numero_1)
     num_2=float(numero_2)
-    operadores_validos='+-/*'
+    operadores_validos='+-/**'
     while operador not in operadores_validos:
         print("Tente novamente, o operador foi considerado invalido.")
         numero_1= input("Digite um numero: ")
         operador= input("Digite um operador: ")
         numero_2= input("Digite outro numero: ")
     if operador == '+':
-        print(num_1 + num_2)
+        print(f'{num_1} + {num_2} = {num_1+num_2}')
     elif operador == '-':
-        print(num_1 - num_2)
+        print(f'{num_1} - {num_2} = {num_1-num_2}')
     elif operador == '*':
-        print(num_1 * num_2)
+        print(f'{num_1} * {num_2} = {num_1*num_2}')
     elif operador == '/':
-        print(num_1 / num_2)
+        print(f'{num_1} / {num_2} = {num_1/num_2}')
+    elif operador == '^':
+        print(f'{num_1} ** {num_2} = {num_1**num_2}')
     sair = input("Quer sair?\n [s]im [n]ao: ").lower().startswith('s')
     if sair is True:
         break
