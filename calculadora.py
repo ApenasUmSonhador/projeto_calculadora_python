6341c0d7f7f944aa8c812b15b71b590ffade2e23
ApenasUmSonhador <123214565+ApenasUmSonhador@users.noreply.github.com>
2023-03-22 15:49:43 -0300

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -10,14 +10,14 @@ while True:
         numero_1= input("Digite um numero: ")
         operador= input("Digite um operador: ")
         numero_2= input("Digite outro numero: ")
-    num_1=float(numero_1)
-    num_2=float(numero_2)
     operadores_validos='+-/*^'
     while operador not in operadores_validos:
         print("Tente novamente, o operador foi considerado invalido.")
         numero_1= input("Digite um numero: ")
         operador= input("Digite um operador: ")
         numero_2= input("Digite outro numero: ")
+    num_1=float(numero_1)
+    num_2=float(numero_2)
     if operador == '+':
         print(f'{num_1} + {num_2} = {num_1+num_2}')
     elif operador == '-':
