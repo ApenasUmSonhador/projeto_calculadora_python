a9f7d21ff6f358b44730cd57a8eeea98d21afcc4
ApenasUmSonhador <123214565+ApenasUmSonhador@users.noreply.github.com>
2023-03-23 11:51:30 -0300

    Update calculadora.py

--- calculadora.py
+++ calculadora.py
@@ -1,3 +1,9 @@
+'''
+Planos para o futuro:
+    1.Criar um manual de uso e printar para o usuário.
+    2.Aceitar outros operadores como: log, sqrt, binomio de newtown,cos, sen,tg,cotg,cossec,tetração,e fatorial.
+    3.Pedir mais ideias para usuários.
+'''
 numero_1=0
 numero_2=0
 operador=''
@@ -28,6 +34,8 @@ while True:
         print(f'{num_1} / {num_2} = {num_1/num_2}')
     elif operador == '^':
         print(f'{num_1} ^ {num_2} = {num_1**num_2}')
+    elif operador == '%':
+        print(f'{num_1} % {num_2} = {num_1%num_2}')
     sair = input("Quer sair?\n [s]im [n]ao: ").lower().startswith('s')
     if sair is True:
         break
